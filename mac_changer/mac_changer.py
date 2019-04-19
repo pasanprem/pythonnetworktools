12931f6232e5c7d6e63aa0cd72c4de91de9d58fa
Pasan <pasan@pasandev.com>
2019-04-19 10:03:13 +0100

    Mac changer

---
+++ mac_changer/mac_changer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import subprocess
+
+subprocess.call("ifconfig eth1 down", shell=True)
+subprocess.call("ifconfig eth1 hw ether 00:11:22:33:44:66", shell=True)
+subprocess.call("ifconfig eth1 up", shell=True)
