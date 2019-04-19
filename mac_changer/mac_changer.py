5270753e1014dc6d1d7103ea422e67afb6871bc2
Pasan <pasan@pasandev.com>
2019-04-19 11:19:10 +0100

    Mac changer - user input variables added secure passing to call function

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -7,7 +7,12 @@ new_mac = input("new MAC > ")
 
 print("[+] Changing the MAC address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
+# The insecure way of call function
+# subprocess.call("ifconfig " + interface + " down", shell=True)
+# subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+# subprocess.call("ifconfig " + interface + " up", shell=True)
 
+# The secure way
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
