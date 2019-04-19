2f34379a6a00ee5f1392291cd4c2e300651312db
Pasan <pasan@pasandev.com>
2019-04-19 12:38:24 +0100

    Mac changer - user input variables added secure passing to call function accepts arguments some error handling added

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -3,24 +3,24 @@
 import subprocess
 import optparse
 
-parser = optparse.OptionParser()
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    (options, arguments) = parser.parse_args()
 
-parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
-parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    if not options.interface:
+        parser.error("[-] Please specify an interface. Use --help for more information.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a MAC. Use --help for more information.")
+    return options
 
-(options, arguments) = parser.parse_args()
+def change_mac(interface, new_mac):
+    print("[+] Changing the MAC address for " + interface + " to " + new_mac)
+    # The necessary shell commands
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
-interface = options.interface
-new_mac = options.new_mac
-
-print("[+] Changing the MAC address for " + interface + " to " + new_mac)
-
-# The insecure way of call function
-# subprocess.call("ifconfig " + interface + " down", shell=True)
-# subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-# subprocess.call("ifconfig " + interface + " up", shell=True)
-
-# The secure way
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
+options = get_arguments()
+change_mac(options.interface, options.new_mac)
