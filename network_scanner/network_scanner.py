2d03a56911a440ff54043f1ff27c168817ea0d36
Pasan <pasan@pasandev.com>
2019-04-19 20:34:37 +0100

    Printing MACs and IPs in a table

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -4,7 +4,19 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    print(arp_request.summary())
+    # arp_request.show()
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    # broadcast.show()
+    # print(broadcast.summary())
+    arp_request_broadcast = broadcast/arp_request
+    # print(arp_request_broadcast.summary())
+    # arp_request_broadcast.show()
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    # print(answered_list.summary())
 
+    print("IP\t\t\tMac Address\n----------------------------------------------------------")
 
-scan ("192.168.1.1")
+    for element in answered_list:
+        print(element[1].psrc +"\t\t" + element[1].hwsrc)
+
+scan("192.168.1.1/24")
